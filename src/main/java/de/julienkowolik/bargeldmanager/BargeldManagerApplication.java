d685e6639ecc2dd68d06cc68b8975f1b46b5228b
Julien Kowolik <nir99jov@uni-duesseldorf.de>
2026-06-07 05:17:28 +0200

    initalisierung

---
+++ src/main/java/de/julienkowolik/bargeldmanager/BargeldManagerApplication.java
@@ -0,0 +1,13 @@
+package de.julienkowolik.bargeldmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BargeldManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BargeldManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/julienkowolik/bargeldmanager/BargeldManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.julienkowolik.bargeldmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BargeldManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
